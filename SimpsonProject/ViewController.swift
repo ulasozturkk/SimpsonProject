3264aa1f79fd6322ee4ba8207702466874d7bb1c
ulasozturk <ulasozturkdev@gmail.com>
2023-11-22 00:55:40 +0300

    Initial Commit

---
+++ SimpsonProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonProject
+//
+//  Created by macbook pro on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
